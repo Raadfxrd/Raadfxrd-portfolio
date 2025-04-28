402ece29fcd66ead36df2b951626018b91b50e14
Borys Babas <borys.babas@hva.nl>
2025-04-28 08:29:57 +0200

    Change CV download button to a link with download attribute
    Converted the CV download button to a downloadable anchor link for improved functionality. Added a new CV file located in the public assets directory.

--- components/WorkExperience.vue
+++ components/WorkExperience.vue
@@ -18,19 +18,22 @@
         </div>
       </div>
     </div>
-    <button
-        class="w-1/2 group relative inline-flex items-center gap-2 px-4 py-2 text-xs bg-button-primary text-text-secondary font-medium rounded-md hover:bg-background-light-2 duration-300 shadow-sm overflow-hidden mt-4">
-        <span class="z-10 relative flex items-center gap-2">
-          <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-               xmlns="http://www.w3.org/2000/svg">
+    <a
+        href="/assets/borys-cv.pdf"
+        download="Borys_CV.pdf"
+        class="w-1/2 group relative inline-flex items-center gap-2 px-4 py-2 text-xs bg-button-primary text-text-secondary font-medium rounded-md hover:bg-background-light-2 duration-300 shadow-sm overflow-hidden mt-4"
+    >
+    <span class="z-10 relative flex items-center gap-2">
+        <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+             xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
                 stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
-          </svg>
-          Download CV
-        </span>
+        </svg>
+        Download CV
+    </span>
       <span class="absolute left-0 top-0 w-full h-full bg-background-light group-hover:animate-shine"></span>
-    </button>
+    </a>
   </div>
 </template>
 
