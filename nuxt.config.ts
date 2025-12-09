c2263e2a0e2c1602188b5327a48f74d5908eddd1
Borys Babas <borys.babas@hva.nl>
2025-12-09 14:19:41 +0100

    Update nuxt.config.ts to clarify experimental collections support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
 
   content: {
     experimental: {
-      // @ts-ignore - collections is supported but not in current type definitions
+      // @ts-expect-error - collections is experimental but supported
       collections: true,
     },
   },
